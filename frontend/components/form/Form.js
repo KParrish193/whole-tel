579c430ff4e2bf2fa316413589dc36388a68aa5c
KParrish <kp@few.io>
2023-06-24 18:48:31 -0600

    form submitting to google sheet

--- frontend/components/form/Form.js
+++ frontend/components/form/Form.js
@@ -1,15 +1,39 @@
 // TODO: add nodemailer
+import { useState } from 'react';
 import styles from "../../styles/form/Form.module.scss";
 import { figtree, metal2 } from "../../styles/fonts";
 import Datepicker from "./datepicker/Datepicker"
 
 const Form = () => {
+    const [submitted, setSubmitted] = useState(false)
+    const [error, setError] = useState(false)
+    const [isSending, setIsSending] = useState()
+
+    if (typeof window !== "undefined") {
+      const contactForm = document.getElementById("contact-form");
+      const scriptURL = process.env.NEXT_PUBLIC_GOOGLE_URL;
+
+      contactForm.addEventListener("submit", (e) => {
+          setIsSending(true);  
+          e.preventDefault();
+          fetch(scriptURL, { method: "POST", body: new FormData(contactForm) })
+          .then((response) => {
+            console.log("Success!", response);
+            setIsSending(false);
+            setSubmitted(true);
+          })
+          .catch((error) => {
+            console.error("Error!", error.message)
+          });
+      });
+    }
+
     return (
       <div className={styles["form-drawer-container"]}>
         <h3 className={styles["contact-header"]} style={metal2.style}>
           Contact Us
         </h3>
-        <form>
+        <form id="contact-form" className={styles}>
           <div className={styles["form-left"]}>
             {/* include to report the date of submission <input type="hidden" /> */}
             <p className={styles["contact-paragraph"]} style={figtree.style}>
@@ -22,10 +46,13 @@ const Form = () => {
             </p>
             <div className={styles["location-wrapper"]}>
               <label style={metal2.style}>Location</label>
-              <select>
+              <select name="Location" required>
                 {/* TODO: dynamically render this from GROQ hook */}
                 {/* <option value="0">Select Property:</option> */}
-                <option value="1">Cabo San Lucas</option>
+                <option value="" disabled hidden selected>
+                  Select a Property
+                </option>
+                <option value="Cabo">Cabo San Lucas</option>
                 {/* <option value="2"></option> */}
               </select>
             </div>
@@ -40,31 +67,66 @@ const Form = () => {
                   type="text"
                   placeholder="First Name"
                   style={figtree.style}
+                  name="First_Name"
+                  aria-label="first name"
                 />
                 <input
                   type="text"
                   placeholder="Last Name"
                   style={figtree.style}
+                  name="Last_Name"
+                  aria-label="last name"
                 />
               </div>
-              <input type="email" placeholder="E-mail" style={figtree.style} />
-              <input type="tel" placeholder="Phone" style={figtree.style} />
+              <input
+                type="email"
+                placeholder="E-mail"
+                style={figtree.style}
+                name="E_mail"
+                aria-label="e-mail"
+              />
+              <input
+                type="tel"
+                placeholder="Phone"
+                style={figtree.style}
+                aria-label="phone number"
+                name="Phone"
+              />
             </div>
           </div>
           <div className={styles["form-right"]}>
             <div className={styles["event-info"]}>
               <label style={metal2.style}>Event Info</label>
-              <input placeholder="Event Date(s)"></input>
-              <select>
-                <option>Number of Guests</option>
+              <input
+                placeholder="Event Date(s)"
+                name="Event_Date"
+                aria-label="event date"
+              ></input>
+              <select name="Guests" aria-label="guests">
+                <option value="" disabled hidden selected>
+                  Number of Guests
+                </option>
+                <option value="1-10">1-10</option>
+                <option value="11-25">11-25</option>
+                <option value="26-50">26-50</option>
+                <option value="50+">50+</option>
+                <option value="Unsure">Not Sure</option>
               </select>
               <textarea
                 placeholder="Tell us about your event"
                 style={figtree.style}
+                name="Message"
+                aria-label="event details"
               />
             </div>
             <div className={styles["submit-wrapper"]}>
-              <input type="submit" value="Send it" style={metal2.style} />
+              <button
+                type="submit"
+                disabled={isSending}
+                style={metal2.style}
+              >
+                Send it
+              </button>
             </div>
           </div>
         </form>
