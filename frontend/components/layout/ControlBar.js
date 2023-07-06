afd24b86fc2ebb9f4387430ca65726b3bafbef74
KParrish <kp@few.io>
2023-07-05 18:50:47 -0600

    added state to click handler

--- frontend/components/layout/ControlBar.js
+++ frontend/components/layout/ControlBar.js
@@ -1,9 +1,10 @@
-import Form from '../form/Form'
-import NavMenu from './NavMenu'
+import { useState } from 'react';
 import styles from "../../styles/layout/ControlBar.module.scss";
 import { figtree, metal2 } from "../../styles/fonts";
 
 import Socials from './Socials';
+import Form from "../form/Form";
+import NavMenu from "./NavMenu";
 
 // Contains JS for Control Bar, Form Drawer and Menu Drawer
 const ControlBar = () => {
@@ -25,7 +26,6 @@ const ControlBar = () => {
     // query selectors for nav and form drawers - Desktop
     const formTrigger = document.getElementById("form-button");
     const navTrigger = document.getElementById("nav-button");
-    const formCloseButton = document.getElementById("form-close-button");
 
     formTrigger.addEventListener("click", () => {
       const formSlider = document.getElementById("form-slider");
@@ -44,19 +44,25 @@ const ControlBar = () => {
       navSlider.classList.toggle(`${styles["is-active"]}`);
       body.classList.toggle("is-clipped-overflow");
     });
-
-    formCloseButton.addEventListener("click", () => {
-      const formSlider = document.getElementById("form-slider");
-      var body = document.querySelector("body");
-      formCloseButton.style.transform = "rotate(360deg)"
-      if(formSlider.classList.contains(`${styles['is-active']}`)){
-        formSlider.classList.remove(`${styles['is-active']}`)
-      }
-      if(body.classList.contains("is-clipped-overflow")){
-        body.classList.remove("is-clipped-overflow")
-      };
-    })
   }
+
+  const [clicked, setClicked] = useState(false);
+  function formClose() {
+    const formSliderGlobal = document.getElementById("form-slider");
+    var bodyGlobal = document.querySelector("body");
+    const formCloseButton = document.getElementById("form-close-button");
+
+    setClicked(true);
+    if (formSliderGlobal.classList.contains(`${styles["is-active"]}`)) {
+      formSliderGlobal.classList.remove(`${styles["is-active"]}`);
+    }
+    if (bodyGlobal.classList.contains("is-clipped-overflow")) {
+      bodyGlobal.classList.remove("is-clipped-overflow");
+    }
+    formCloseButton.style.transform = "rotate(360deg)";
+    setClicked(false);
+  }
+  console.log("clicked state", clicked);
 
   return (
     <div>
@@ -88,7 +94,7 @@ const ControlBar = () => {
       </div>
 
       <div className={styles["form-slider"]} id="form-slider">
-        <button className={styles["form-close-button"]} id="form-close-button">
+        <button className={styles["form-close-button"]} id="form-close-button" onClick={formClose}>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </button>
